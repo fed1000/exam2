a98cfd6caa04ce4bbadf8472aca141c7d0dea740
Fedot Shmakov <shmakov@gigansk.ru>
2022-02-10 15:44:32 +0000

    [ex2-88] Оценить скорость работы сайта – страницы и созданный простой компонент

---
+++ ex2/time_control/index.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>Самая ресурсоемкая страница: /contacts/index.php Доля нагрузки:&nbsp;15.85%<br>
+ <br>
+ Размер кеша в компоненте news.detail по умолчанию&nbsp;7 КБ<br>
+ <br>
+ Работа кеша в компоненте, только нужные параметры&nbsp; -&nbsp;&nbsp;4 КБ.<br>
+ <br>
+ Разница 7 КБ - 4 КБ = 3 КБ<br>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
